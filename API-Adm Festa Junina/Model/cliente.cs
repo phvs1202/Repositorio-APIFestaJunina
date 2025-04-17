c8e391f994f242fb2c70e3079c5c9c8571576b10
HTML <121892240+ErickAnthonyNovaes@users.noreply.github.com>
2025-04-17 17:21:48 -0300

    Erro pequeneninho
    Troquei o string do telefone ,para int, assim como esta no BD da Festa Junina.

--- API-Adm Festa Junina/Model/cliente.cs
+++ API-Adm Festa Junina/Model/cliente.cs
@@ -6,6 +6,6 @@
         public string nome{ get; set; }
         public string email { get; set; }
         public string senha { get; set; }
-        public string telefone { get; set; }
+        public int telefone { get; set; }
     }
 }
