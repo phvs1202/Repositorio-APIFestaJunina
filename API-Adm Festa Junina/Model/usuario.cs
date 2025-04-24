85c0e45050ab100695c52e817991ed66343c4cb3
Iago Henrique Taveira Bueno <iagohtb@gmail.com>
2025-04-24 17:22:05 -0300

    CORS

--- API-Adm Festa Junina/Model/usuario.cs
+++ API-Adm Festa Junina/Model/usuario.cs
@@ -6,10 +6,10 @@ namespace API_Adm_Festa_Junina.Model
     {
         public int id { get; set; }
         public string nome { get; set; }
-        public string caminho_foto { get; set; }
+        public string? caminho_foto { get; set; }
         public string email { get; set; }
         public string senha { get; set; }
-        public string telefone {get; set;}
+        public string telefone { get; set;}
         public int perfil_id { get; set; }
         
     }
--- API-Adm Festa Junina/Program.cs
+++ API-Adm Festa Junina/Program.cs
@@ -5,18 +5,18 @@ System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolTyp
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração de CORS
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
+    options.AddDefaultPolicy(policy =>
     {
-        builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,14 +32,16 @@ builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Habilita o CORS antes de qualquer middleware que possa bloquear a requisição
 app.UseCors();
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
